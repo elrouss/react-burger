4366737a3a3f09859e189e95e903c2a7b2259526
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-06-04 21:56:45 +0300

    feat: upload data with ingredients from api

---
+++ src/components/App/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+import AppHeader from '../AppHeader/AppHeader';
+import Shop from '../Shop/Shop';
+
+import API from '../../utils/constants';
+
+import './App.module.scss';
+
+function App() {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    (async function getIngredients() {
+      try {
+        const res = await fetch(API.ingredients);
+        const success = await res.json();
+
+        if (success) {
+          setIngredients(success.data);
+        } else {
+          throw new Error();
+        }
+      } catch (err) {
+        console.error(
+          `Ошибка в процессе получения данных об ингредиентах с сервера: ${err}`
+        );
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <AppHeader />
+      <Shop data={ingredients} />
+    </>
+  );
+}
+
+export default App;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,14 +3,14 @@ import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
 
 import styles from './Shop.module.scss';
 
-function Shop() {
+function Shop(props) {
   return (
     <main className={styles.main}>
       <div className={styles.wrapper}>
         <h1 className={styles.heading}>Соберите бургер</h1>
         <div className={styles.shop}>
-          <BurgerIngredients />
-          <BurgerConstructor />
+          <BurgerIngredients {...props} />
+          <BurgerConstructor {...props} />
         </div>
       </div>
     </main>
