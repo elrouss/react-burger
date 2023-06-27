3bd4809b1984b323d4673aad5ba7e3fff2312fe1
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-06-27 18:55:13 +0300

    feat: mark up desktop reset-password

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import HomePage from '../../pages/home';
 import RegisterPage from '../../pages/register';
 import LoginPage from '../../pages/login';
 import ForgotPasswordPage from '../../pages/forgot-password';
+import ResetPasswordPage from '../../pages/reset-password';
 
 import { ROUTES } from '../../utils/constants';
 
@@ -14,6 +15,7 @@ function App() {
       <Route path={ROUTES.sign.up} element={<RegisterPage />} />
       <Route path={ROUTES.sign.in} element={<LoginPage />} />
       <Route path={ROUTES.password.forgot} element={<ForgotPasswordPage />} />
+      <Route path={ROUTES.password.reset} element={<ResetPasswordPage />} />
     </Routes>
   );
 }
---
+++ src/pages/reset-password.jsx
@@ -0,0 +1,7 @@
+import ResetPassword from '../components/reset-password/reset-password';
+
+function ResetPasswordPage() {
+  return <ResetPassword />;
+}
+
+export default ResetPasswordPage;
